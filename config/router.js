cb047cc648dadeaff3a7cc213762dc164db606a8
nejat-njonjo <nejatmhango@gmail.com>
2021-01-04 15:09:20 +0200

    added root user route

--- config/router.js
+++ config/router.js
@@ -1,1 +1,12 @@
-const router = require('express').Router
+const router = require('express').Router()
+
+/**
+ * Controllers
+ */
+const userController = require('../controllers/user-controller')
+/**
+ * Users
+ */
+router.get('/users', userController.getUsers)
+
+module.exports = router
