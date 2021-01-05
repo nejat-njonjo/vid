edd61b0c6f3fd25d1bbbb4e15ecc7c59922d382c
nejat-njonjo <nejatmhango@gmail.com>
2021-01-05 15:58:19 +0200

    configured routes and views

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const config = require('./config')
 
 const app = express();
 
@@ -19,8 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', config.router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
