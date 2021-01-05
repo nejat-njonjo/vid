a2862d2fd29067b142f6e7cf4e21400de25248aa
nejat-njonjo <nejatmhango@gmail.com>
2021-01-05 15:58:48 +0200

    aaded controllers

--- config/router.js
+++ config/router.js
@@ -4,6 +4,13 @@ const router = require('express').Router()
  * Controllers
  */
 const userController = require('../controllers/user-controller')
+const chatController = require('../controllers/chat-controller')
+
+/**
+ * Chats
+ */
+router.get('/', chatController.getChats)
+
 /**
  * Users
  */
