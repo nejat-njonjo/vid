8fe40cb0e4cbc1a47fad1f53a96a12966cd50852
nejat-njonjo <nejatmhango@gmail.com>
2021-01-14 19:12:02 +0200

    added client io

--- config/router.js
+++ config/router.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const path = require('path')
 
 /**
  * Controllers
@@ -15,5 +16,15 @@ router.get('/', chatController.getChats)
  * Users
  */
 router.get('/users', userController.getUsers)
+
+router.get('/socket.io.min.js', (req, res) => {
+  const file = `${__dirname}/../node_modules/socket.io/client-dist/socket.io.min.js`
+  res.sendFile(path.resolve(file))
+})
+
+router.get('/socket.io.min.js.map', (req, res) => {
+  const file = `${__dirname}/../node_modules/socket.io/client-dist/socket.io.min.js.map`
+  res.sendFile(path.resolve(file))
+})
 
 module.exports = router
