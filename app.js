2868be52402cc75f13e80f4fb51c265d7932181f
nejat-njonjo <nejatmhango@gmail.com>
2021-02-06 23:45:25 +0200

    added cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const config = require('./config')
+const cors = require('cors')
 
 const app = express();
 
